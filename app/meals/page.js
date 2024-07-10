41dda02b1c190cbacc105928725c54fcdf9ae14e
Alexakis, Geogios <Georgios.Alexakis@arhs-dev-hellas.com>
2024-07-10 13:35:55 +0300

    Setup db and fetch data with loading

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,11 +1,35 @@
+import MealsGrid from "@/components/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
+import classes from "./page.module.css";
 import Link from "next/link";
+import { Suspense } from "react";
+import MealsLoadingPage from "./loading-out";
+
+const Meals = async () => {
+  const meals = await getMeals();
+
+  return <MealsGrid meals={meals} />;
+};
 
 export default function MealsPage() {
   return (
     <>
-      <h1>Meals page</h1>
-      <Link href="/slug1">Slug 1</Link>
-      <Link href="/slug2">Slug 2</Link>
+      <haeder className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+
+        <p>Choose your favorite recipe</p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share your favorite recipe</Link>
+        </p>
+      </haeder>
+      <main className={classes.main}>
+        <Suspense fallback={<MealsLoadingPage />}>
+          <Meals />
+        </Suspense>
+      </main>
     </>
   );
 }
