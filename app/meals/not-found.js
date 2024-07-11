1aff75496fc498d5d1b9281a5e51220dca9168dc
Alexakis, Geogios <Georgios.Alexakis@arhs-dev-hellas.com>
2024-07-11 13:35:06 +0300

    Created error pages not-found pages and form submitting services and pages

---
+++ app/meals/not-found.js
@@ -0,0 +1,9 @@
+const MealNotFound = () => {
+  return (
+    <main className="not-found">
+      <h1>Meal not found</h1>
+    </main>
+  );
+};
+
+export default MealNotFound;
---
+++ app/not-found.js
@@ -0,0 +1,10 @@
+const NotFoundPage = () => {
+  return (
+    <main className="not-found ">
+      <h1>Not found</h1>
+      <p>Look somewhere else bro !</p>
+    </main>
+  );
+};
+
+export default NotFoundPage;
